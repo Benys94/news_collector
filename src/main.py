b6585fcd61d8c120506053433d915825a10f883c
David Benes <benys94@gmail.com>
2019-06-16 23:21:37 +0200

    Code reorganized.

--- src/main.py
+++ src/main.py
@@ -5,44 +5,8 @@
 .. Created on 2019-06-15
 """
 
-import requests
-
-
-class RequestSender:
-    """
-    Sending a HTTP requests to certain web pages
-
-    :param str address:
-        URL of web service
-    :param int port:
-        Port of web service. This argument is optional
-    :param str schema:
-        Schema of web service. HTTP is default
-    """
-
-    def __init__(self, address, port=None, schema="http"):
-        tmp_port = ":{}".format(port) if port is not None else ""
-        self._api_address = "{schema}://{address}{port}".format(
-            schema=schema, address=address, port=tmp_port
-        )
-
-    def send_req(self, path, payload=None):
-        """
-        Sends given data to web API
-
-        :param str path:
-            API path to be reached
-        :param payload:
-            Data to be sent
-        :return:
-            Service's response
-        :rtype: dict
-        """
-        full_path = "{url}/{path}".format(
-            url=self._api_address, path=path
-        )
-        response = requests.get(full_path, payload)
-        return response.json()
+from src.api_interface import APIHandler
+from src.api_interface import NewStoriesAPI
 
 
 def parse_response(response_data):
@@ -56,9 +20,10 @@ def parse_response(response_data):
 
 
 if __name__ == '__main__':
-    API_CLIENT = RequestSender(
+    API_CLIENT = APIHandler(
+        worker=NewStoriesAPI(),
         address='hacker-news.firebaseio.com/v0',
         schema='https'
     )
-    RES_DATA = API_CLIENT.send_req(path='newstories.json')
+    RES_DATA = API_CLIENT.get_api_data()
     parse_response(RES_DATA)
